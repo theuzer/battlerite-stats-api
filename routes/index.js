b4ea3b43d1a050d3722b4e6320478e85385fbd47
theuzer <jarvandemacio@gmail.com>
2018-02-27 16:11:16 +0000

    devs

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
 const statsController = require('../controllers/statsController');
+const playerController = require('../controllers/playerController');
+
 const characterController = require('../controllers/characterController');
 
 const router = express.Router();
 
 router.get('/stats', statsController.getStatsApi);
-router.get('/player/exists', characterController.getPlayer);
+router.get('/player/exists', playerController.checkIfPlayerExistsApi);
 
 module.exports = router;
