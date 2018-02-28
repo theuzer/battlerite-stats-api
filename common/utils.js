a5eeb1a51d86f53f18ddcaecd5d7b2e2bd69f7a2
theuzer <jarvandemacio@gmail.com>
2018-02-28 17:07:48 +0000

    test

--- common/utils.js
+++ common/utils.js
@@ -9,43 +9,45 @@ const english = iniparser.parseSync('./static/assets/English.ini');
 const getBattleriteInfo = (battleriteCode, championInfo) => championInfo.battlerites.filter(x => x.typeID === battleriteCode)[0];
 
 exports.getMatchInformation = (match) => {
-  console.log(1, match);
-  const mapInfo = stackables.Mappings.filter(x => x.LocalizedName === map.filter(y => y.mapLogId === match.MapId)[0].mapStackablesId)[0];
-  console.log(2, mapInfo);
-  const championInfo = gameplay.characters.filter(x => x.typeID === match.ChampionCode)[0];
-  console.log(3, championInfo);
-  const battlerite1Info = getBattleriteInfo(match.Br1, championInfo);
-  const battlerite2Info = getBattleriteInfo(match.Br2, championInfo);
-  const battlerite3Info = getBattleriteInfo(match.Br3, championInfo);
-  const battlerite4Info = getBattleriteInfo(match.Br4, championInfo);
-  const battlerite5Info = getBattleriteInfo(match.Br5, championInfo);
+  try {
+    const mapInfo = stackables.Mappings.filter(x => x.LocalizedName === map.filter(y => y.mapLogId === match.MapId)[0].mapStackablesId)[0];
+    const championInfo = gameplay.characters.filter(x => x.typeID === match.ChampionCode)[0];
+    const battlerite1Info = getBattleriteInfo(match.Br1, championInfo);
+    const battlerite2Info = getBattleriteInfo(match.Br2, championInfo);
+    const battlerite3Info = getBattleriteInfo(match.Br3, championInfo);
+    const battlerite4Info = getBattleriteInfo(match.Br4, championInfo);
+    const battlerite5Info = getBattleriteInfo(match.Br5, championInfo);
 
-  return {
-    map: {
-      iconId: mapInfo.Image,
-      name: english[mapInfo.LocalizedName],
-    },
-    champion: {
-      iconId: championInfo.icon,
-      name: english[championInfo.name],
-    },
-    loadout: [{
-      iconId: battlerite1Info.icon,
-      name: english[battlerite1Info.name],
-    }, {
-      iconId: battlerite2Info.icon,
-      name: english[battlerite2Info.name],
-    }, {
-      iconId: battlerite3Info.icon,
-      name: english[battlerite3Info.name],
-    }, {
-      iconId: battlerite4Info.icon,
-      name: english[battlerite4Info.name],
-    }, {
-      iconId: battlerite5Info.icon,
-      name: english[battlerite5Info.name],
-    }],
-  };
+    return {
+      map: {
+        iconId: mapInfo.Image,
+        name: english[mapInfo.LocalizedName],
+      },
+      champion: {
+        iconId: championInfo.icon,
+        name: english[championInfo.name],
+      },
+      loadout: [{
+        iconId: battlerite1Info.icon,
+        name: english[battlerite1Info.name],
+      }, {
+        iconId: battlerite2Info.icon,
+        name: english[battlerite2Info.name],
+      }, {
+        iconId: battlerite3Info.icon,
+        name: english[battlerite3Info.name],
+      }, {
+        iconId: battlerite4Info.icon,
+        name: english[battlerite4Info.name],
+      }, {
+        iconId: battlerite5Info.icon,
+        name: english[battlerite5Info.name],
+      }],
+    };
+  } catch (e) {
+    console.log(e);
+    return e;
+  }
 };
 
 exports.getTimePeriodFilter = (timePeriodFilter) => {
